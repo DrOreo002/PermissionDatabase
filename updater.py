bd7715c9ec35da124bbaa82912f44d1eb376842b
DrOreo002 <bagusanomsurya1@yahoo.com>
2019-08-31 23:21:26 +0800

    Big database update

--- updater.py
+++ updater.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 import string
 import fileinput
+
+dateTimeObj = datetime.now()
 
 PERM_OWNER = 0
 PERMISSION = 1
@@ -10,9 +13,10 @@ PERM_DEFAULT_ASIGNMENT = 4
 EXPECTED_SIZE = 5
 
 f = open("Database.txt", "r+")
+target = open("Database Update (" + dateTimeObj.strftime("%d-%b-%Y") + ")" + ".txt", "a+")
 for line in f.readlines():
     if line.startswith("#"):
-        f.write(line)
+        target.write(line)
         continue
     line = line.replace("\n", "")
     x = line.split("|")
@@ -20,14 +24,24 @@ for line in f.readlines():
 
     # If there's only <pluginName>|<permission>
     if size == 2:
-        f.write(x[PERM_OWNER] + "|" + x[PERMISSION] + "|empty|empty|op\n")
+        target.write(x[PERM_OWNER] + "|" + x[PERMISSION] + "|empty|empty|op\n")
         continue
     # If there's only <pluginName>|<permission>|<desc>
     if size == 3: 
-        f.write(x[PERM_OWNER] + "|" + x[PERMISSION] + "|" + x[PERM_DESC] + "|empty|op\n")
+        target.write(x[PERM_OWNER] + "|" + x[PERMISSION] + "|" + x[PERM_DESC] + "|empty|op\n")
         continue
     # If there's only <pluginName>|<permission>|<desc>|<commands>    
     if size == 4:
-        f.write(x[PERM_OWNER] + "|" + x[PERMISSION] + "|" + x[PERM_DESC] + "|" + x[PERM_COMMANDS] + "|op\n")
+        commandConverted = "<l>" + x[PERM_COMMANDS].replace(",", "<br>") + "<l>"
+        target.write(x[PERM_OWNER] + "|" + x[PERMISSION] + "|" + x[PERM_DESC] + "|" + commandConverted + "|op\n")
         continue
+    # If complete
+    if size == EXPECTED_SIZE:
+        commandConverted = x[PERM_COMMANDS]
+        if not "<l>" in commandConverted: # This means, not contains
+            commandConverted = "<l>" + x[PERM_COMMANDS].replace(",", "<br>") + "<l>"
+        target.write(x[PERM_OWNER] + "|" + x[PERMISSION] + "|" + x[PERM_DESC] + "|" + commandConverted + "|" + x[PERM_DEFAULT_ASIGNMENT] + "\n")
+        continue
+# End of method
+target.close()
 f.close()

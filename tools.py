51507f7bdb2c5cb5fc425de6dc8d2daf6512cb43
DrOreo002 <bagusanomsurya1@yahoo.com>
2019-10-18 20:53:58 +0800

    Final update?

--- tools.py
+++ tools.py
@@ -1,3 +1,9 @@
+# Python3 program that contains useful tools
+# to manage permission database, currently for offline usage only
+# this will be removed later if the website is up
+# 
+# 
+# Made by DrOreo002
 from datetime import datetime
 from colorama import Fore
 from colorama import Style
@@ -109,50 +115,69 @@ def update_icon():
 # dependending the value specified
 def update_player_head():
     p_heads = {}
+    updated_heads = {}
     line_number = 0
     icon_data = open("IconData.txt", "r+")
     for line in icon_data.readlines():
         if "PLAYER_HEAD" in line:
-            if len(line.split("|")) > 2:
-                continue # Already has texture
-            # This rstrip will remove the \n from the line
-            p_heads[line.rstrip()] = line_number # Add to hashmap 
+            if not len(line.split("|")) > 2:
+                # This rstrip will remove the \n from the line
+                p_heads[line.rstrip()] = line_number # Add to hashmap 
         line_number += 1
+
+    if len(p_heads) == 0:
+        print(INFO_TEXT + "No update-able heads found!")
+        quit(0)
+        pass
 
     for key in list(p_heads.keys()):
         print(INFO_TEXT + "Found PLAYER_HEAD on line " + Fore.RED + str(p_heads.get(key)) + Fore.WHITE + " is > " + Fore.GREEN + key + Fore.RESET)
 
     print(Fore.MAGENTA + "------------------------------------")
 
+    allData = True
     for key in list(p_heads.keys()):
         print(INFO_TEXT + "Please type texture for " + Fore.RED + key + Fore.WHITE + " (Type SAVE to quit and save)")
         texture = input("       > ")
         if texture == "SAVE":
-            for key in list(p_heads.keys()):
-                print(INFO_TEXT + "Saving " + Fore.RED + key + Fore.WHITE + " into line number " + str(p_heads.get(key)))
+            allData = False
+            for key in list(updated_heads.keys()):
+                print(INFO_TEXT + "Saving " + Fore.RED + key + Fore.WHITE + " into line number " + str(updated_heads.get(key)))
             break
 
         l_number = p_heads.get(key)
-        p_heads.pop(key)
-        p_heads[key + "|" + texture] = l_number
+        updated_heads[key + "|" + texture] = l_number
+
+    if allData:
+        for key in list(updated_heads.keys()):
+            print(INFO_TEXT + "Saving " + Fore.RED + key + Fore.WHITE + " into line number " + str(updated_heads.get(key)))
+
+    f_data = []
+
+    icon_data.seek(0)
+    line_number = 0
+
+    for line in icon_data.readlines():
+        for key, val in updated_heads.items():
+            # There's a change. We use different assignment then
+            if line.split("|")[0] == key.split("|")[0]:
+                f_data.append(key + "\n")
+                line_number += 1
+                break
+            else:
+                f_data.append(line)
+                line_number += 1
+                break
+        line_number += 1
 
     # Replace the lines
     icon_data.close()
     open("IconData.txt", "w").close(); # Clear the file!
     icon_data = open("IconData.txt", "r+")
 
-    f_data = []
-
-    for line in icon_data.readlines():
-        f_data.append(line)
-
-    # HOLY SHIT ERROR!!!
-    for changes in list(p_heads.keys()):
-        f_data[p_heads.get(changes)] = changes + "\n" # Add the new line back
-    
     # Write to file
     icon_data.writelines(f_data)
-    icon_data.flush
+    icon_data.flush()
     icon_data.close()
     print(Fore.MAGENTA + "------------------------------------")
     print(INFO_TEXT + "Data saved successfully...")
@@ -162,6 +187,9 @@ def update_player_head():
 def show_information():
     version = "UNKNOWN"
     perm_owners = []
+    perm_icon = []
+
+    icon_data = open("IconData.txt", "r+")
 
     for line in f.readlines():
         pl_name = line.split("|")[0]
@@ -169,7 +197,14 @@ def show_information():
             continue
         perm_owners.append(pl_name)
 
+    for line in icon_data.readlines():
+        pl_name = line.split("|")[0]
+        if pl_name in perm_icon:
+            continue
+        perm_icon.append(pl_name)
+
     f.seek(0)
+    icon_data.seek(0)
 
     for line in f.readlines():
         if "Version" in line and line.startswith("#"):
@@ -179,13 +214,20 @@ def show_information():
     f.seek(0) # Reset to beginning
     data_size = sum(1 for l in f.readlines())
     perm_owners_size = sum(1 for p in perm_owners)
+    icon_size = sum(1 for icon in perm_icon)
 
-    print(Fore.GREEN + "[!]" + Fore.WHITE + " Data information for database version " + Fore.RED + "v" +  version + Fore.RESET, end = '')
-    print("--")
-    print(Fore.GREEN + "[!]" + Fore.WHITE + " Data size > " + str(data_size) + " lines" + Fore.RESET)
-    print(Fore.GREEN + "[!]" + Fore.WHITE + " Loaded permission owner > " + str(perm_owners_size))
-    print("--")
-    print(Fore.GREEN + "[!]" + Fore.WHITE + " End of data information. More will be given in the future" + Fore.RESET)
+    print(INFO_TEXT + "Data information for database version " + Fore.RED + "v" +  version + Fore.RESET, end = '')
+    print("-------")
+    print(INFO_TEXT + "Data size > " + Fore.RED + str(data_size) + " lines" + Fore.RESET)
+    print(INFO_TEXT + "Loaded permission owner > " + Fore.RED + str(perm_owners_size))
+    print(INFO_TEXT + "Loaded permission owner > " + Fore.RED + str(perm_owners_size) + Fore.RESET)
+    print("-------")
+    print(INFO_TEXT + "End of data information. More will be given in the future" + Fore.RESET)
+
+    # End
+    f.close()
+    target.close()
+    icon_data.close()
     pass
 
 # Will check for duplicated icons
@@ -197,6 +239,7 @@ def check_duplicated_icon():
     last_owner = None
 
     for line in icon_data.readlines():
+        line = line.rstrip()
         dat = line.split("|")
         if (dat[1] in loaded_data):
             loaded_data[dat[1]] = loaded_data.get(dat[1]) + 1
@@ -204,15 +247,41 @@ def check_duplicated_icon():
             loaded_data[dat[1]] = 1
 
     for data in loaded_data:
-        print("[!] " + data + " > " + str(loaded_data[data]))
-
-    print(" ")
-    print("[!] Data that is more than 2 duplicates [!]")
-    print(" ")
-
-    for data in loaded_data:
         if loaded_data[data] > 2:
-            print("[!] " + data + " > " + str(loaded_data[data]))
+            print(INFO_TEXT + data + " has in total of " + Fore.RED + str(loaded_data[data]) + Fore.WHITE + " duplicates")
+
+    # Save and close
+    icon_data.flush()
+    icon_data.close()
+
+    target.flush()
+    target.close()
+
+    # Close only, because we only reads the data
+    f.close()
+
+def check_duplicated_icon_name():
+    print(INFO_TEXT + "Checking for duplicated icon name...")
+
+    icon_data = open("IconData.txt", "r+")
+    loaded_data = {}
+
+    for line in icon_data.readlines():
+        dat = line.split("|")
+        if dat[0] in loaded_data.keys():
+            loaded_data[dat[0]] = loaded_data.get(dat[0]) + 1
+        else:
+            loaded_data[dat[0]] = 1
+
+    hasDuplicate = False
+
+    for key, val in loaded_data.items():
+        if val > 1:
+            hasDuplicate = True
+            print(Fore.WHITE + "     > Found duplicate for data " + key + Fore.GREEN + "(" + str(val) + ")")
+
+    if not hasDuplicate:
+        print(INFO_TEXT + "No duplicated icon name found. Hooray!")
 
     # Save and close
     icon_data.flush()
@@ -243,8 +312,12 @@ if (command.lower() == "-update-icon"):
     update_icon()
     found = True
     pass
-if (command.lower() == "-check-for-duplicate"):
+if (command.lower() == "-check-for-duplicated-icon"):
     check_duplicated_icon()
+    found = True
+    pass
+if (command.lower() == "-check-for-duplicated-icon-name"):
+    check_duplicated_icon_name()
     found = True
     pass
 if (command.lower() == "-info"):
